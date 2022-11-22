688ff68ba21e582cb746b261cdc458824c15e0ed
Roland Toth <roland_toth@epam.com>
2022-11-22 12:09:11 +0100

    Homework-1

---
+++ src/homework-1/task-2.ts
@@ -0,0 +1,22 @@
+import fs from "fs";
+import csvToJson from "csvtojson/v2";
+
+interface IBook {
+  Book: string;
+  Author: string;
+  Amount: string;
+  Price: string;
+}
+
+const csvPath = `${__dirname}/csv/task-2-data.csv`;
+const onError = (err) => console.log(err);
+const writeTxtStream = fs.createWriteStream("output.txt", {
+  flags: "a",
+  encoding: "utf8",
+});
+
+csvToJson({ delimiter: ";" })
+  .fromStream(fs.createReadStream(csvPath))
+  .subscribe((json: IBook) => {
+    writeTxtStream.write(JSON.stringify(json) + "\n");
+  }, onError);
